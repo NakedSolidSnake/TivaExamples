6f97795eb19dc3de148cb979c553b59253b0b5c3
NakedSolidSnake <cristianosstec@gmail.com>
2018-08-21 11:16:46 -0300

    Cooperative Scheduler

---
+++ Keil Compiler/03_Non_Preemptive_Scheduler/main.c
@@ -0,0 +1,40 @@
+#include "osKernel.h"
+
+#define QUANTA 10
+
+uint32_t count0 = 0, count1 = 0, count2 = 0;
+
+void Task0(void)
+{
+	while(1)
+	{
+		count0++;
+		osThreadYield();
+	}
+}
+
+void Task1(void)
+{
+	while(1)
+	{
+		count1++;
+		
+	}
+}
+
+void Task2(void)
+{
+	while(1)
+	{
+		count2++;
+	}
+}
+
+int main(void)
+{
+	osKernelInit();
+	osKernelAddThreads(&Task0, &Task1, &Task2);
+	osKernelLaunch(QUANTA);
+	
+	while(1);
+}
