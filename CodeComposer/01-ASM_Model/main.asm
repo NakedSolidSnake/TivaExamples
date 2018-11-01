8ed1a37169cb359e9b04bd8067809423458adfa9
NakedSolidSnake <cristianosstec@gmail.com>
2018-11-01 18:17:10 -0300

    Arm ASM Template

---
+++ CodeComposer/01-ASM_Model/main.asm
@@ -0,0 +1,7 @@
+	.global main								; makes main acessible from outside this file
+	.thumb										; use thumb
+	.data                                       ; set memory location to sram
+	;put here your variables
+	.text										; set memory location to flash
+main:
+	; code goes here
