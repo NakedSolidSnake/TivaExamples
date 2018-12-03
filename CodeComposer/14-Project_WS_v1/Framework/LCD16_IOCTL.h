9957d029d312cf3da2649ebafa84e8579b408129
NakedSolidSnake <cristianosstec@gmail.com>
2018-12-03 18:20:30 -0200

    LCD Ioctl

---
+++ CodeComposer/14-Project_WS_v1/Framework/LCD16_IOCTL.h
@@ -0,0 +1,17 @@
+/*
+ * LCD16_IOCTL.h
+ *
+ *  Created on: 3 de dez de 2018
+ *      Author: csouza
+ */
+
+#ifndef FRAMEWORK_LCD16_IOCTL_H_
+#define FRAMEWORK_LCD16_IOCTL_H_
+
+
+typedef enum _LCD16_IOCTL{
+
+}LCD16_Ioctl;
+
+
+#endif /* FRAMEWORK_LCD16_IOCTL_H_ */
